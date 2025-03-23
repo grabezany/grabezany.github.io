f730a2ea929077012f96e06d754b1a37fc35ff80
Gabriel <grabezanany@gmail.com>
2025-03-22 22:25:21 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+var version = "1.0"
+
+function getVersion(){
+  if(data.version != version){
+    var ask = confirm("Looks like you're not on the latest version of Fishing Simulator. Would you like to update to version" + version);
+  }
+}
